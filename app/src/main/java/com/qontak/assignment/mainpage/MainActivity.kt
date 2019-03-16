a74476d693a43164e3bf68aea5f7d234e47b16e8
Udin <udines@users.noreply.github.com>
2019-03-16 22:39:00 +0700

    Rename linearLayoutManager into gridLayoutManager.

--- app/src/main/java/com/qontak/assignment/mainpage/MainActivity.kt
+++ app/src/main/java/com/qontak/assignment/mainpage/MainActivity.kt
@@ -3,23 +3,27 @@ package com.qontak.assignment.mainpage
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.view.Menu
 import com.qontak.assignment.Constants
 import com.qontak.assignment.datamodel.Movie
 import kotlinx.android.synthetic.main.activity_main.*
+import com.qontak.assignment.R
+import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity(), MainView {
 
-    private lateinit var linearLayoutManager: GridLayoutManager
+    private lateinit var gridLayoutManager: GridLayoutManager
     private lateinit var adapter: MovieListAdapter
     private lateinit var mainPresenter: MainActivityPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.qontak.assignment.R.layout.activity_main)
+        setSupportActionBar(main_toolbar)
 
-        linearLayoutManager = GridLayoutManager(this, 2)
-        main_recycler_view.layoutManager = linearLayoutManager
+        gridLayoutManager = GridLayoutManager(this, 2)
+        main_recycler_view.layoutManager = gridLayoutManager
 
         mainPresenter = MainActivityPresenter(this, MainActivityInteractor())
 
