0b41b7bbba4b46b84b286efd48d49f07722caf7e
Udin <udines@users.noreply.github.com>
2019-03-16 19:45:18 +0700

    Create Constants class

---
+++ app/src/main/java/com/qontak/assignment/Constants.kt
@@ -0,0 +1,9 @@
+package com.qontak.assignment
+
+class Constants() {
+    companion object {
+        const val API_KEY = "f6cd08f3369460bc00695cfeda1f1a28"
+        const val FILTER_TOP_RATED = "top_rated"
+        const val FILTER_POPULAR = "popular"
+    }
+}
