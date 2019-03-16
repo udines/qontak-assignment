7d524d85eed8a1d6db3a12d6aa155a96fe4dbb81
Udin <udines@users.noreply.github.com>
2019-03-16 21:41:09 +0700

    Change API url to get popular movies.

--- app/src/main/java/com/qontak/assignment/mainpage/MainActivityInteractor.kt
+++ app/src/main/java/com/qontak/assignment/mainpage/MainActivityInteractor.kt
@@ -14,7 +14,7 @@ class MainActivityInteractor {
 
     fun getPopularMovies(onFinishedListener: OnFinishedListener, page: Int) {
 
-        val url = "https://api.themoviedb.org/3/discover/movie?api_key=" + Constants.API_KEY + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + page.toString()
+        val url = "https://api.themoviedb.org/3/movie/popular?api_key=" + Constants.API_KEY + "&language=en-US&page=" + page.toString()
 
         val client = OkHttpClient()
         val request = Request.Builder()
