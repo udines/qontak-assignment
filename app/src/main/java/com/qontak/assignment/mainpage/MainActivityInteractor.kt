1bef8bf56057bb4a0dda56334b6ebaa4b52eb7fb
Udin <udines@users.noreply.github.com>
2019-03-16 21:44:26 +0700

    Add function to get top rated movies in main activity Interactor.

--- app/src/main/java/com/qontak/assignment/mainpage/MainActivityInteractor.kt
+++ app/src/main/java/com/qontak/assignment/mainpage/MainActivityInteractor.kt
@@ -39,6 +39,33 @@ class MainActivityInteractor {
         })
     }
 
+    fun getTopRatedMovies(onFinishedListener: OnFinishedListener, page: Int) {
+
+        val url = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + Constants.API_KEY + "&language=en-US&page=" + page.toString()
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .get()
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("response code", response.code().toString())
+                val jsonData = response.body()?.string()
+                if (jsonData != null) {
+                    onFinishedListener.onResultSuccess(jsonData)
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                onFinishedListener.onResultFail()
+            }
+
+        })
+    }
+
     fun searchMovieByTitle(onFinishedListener: OnFinishedListener, page: Int, title: String) {
         val url = "https://api.themoviedb.org/3/search/movie?api_key=" + Constants.API_KEY + "&language=en-US&query=" + title + "&page=" + page.toString() + "&include_adult=false"
 
