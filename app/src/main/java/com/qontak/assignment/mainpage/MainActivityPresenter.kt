4bc6c9ad993610ba0d9e3f9fd7285247080cc38d
Udin <udines@users.noreply.github.com>
2019-03-16 20:47:28 +0700

    Create function to convert json result into Movie object in main activity Presenter.

---
+++ app/src/main/java/com/qontak/assignment/mainpage/MainActivityPresenter.kt
@@ -0,0 +1,48 @@
+package com.qontak.assignment.mainpage
+
+import android.util.Log
+import com.google.gson.Gson
+import com.qontak.assignment.Constants
+import com.qontak.assignment.datamodel.Movie
+import org.json.JSONObject
+
+
+
+class MainActivityPresenter(private var mainView: MainView?, private val mainInteractor: MainActivityInteractor)
+    : MainActivityInteractor.OnFinishedListener {
+
+    override fun onResultSuccess(jsonData: String) {
+        mainView?.showList(convertJsonToArrayList(jsonData))
+    }
+
+    override fun onResultFail() {
+        Log.e("result", "fail")
+    }
+
+    fun getData(page: Int, filter: String) {
+        when (filter) {
+            Constants.FILTER_POPULAR -> mainInteractor.getPopularMovies(this, page)
+        }
+    }
+
+    fun convertJsonToArrayList(jsonData: String): ArrayList<Movie> {
+
+        val jsonObject = JSONObject(jsonData)
+        val jsonArray = jsonObject.getJSONArray("results")
+
+        val movieList = ArrayList<Movie>()
+
+        for (i in 0 until jsonArray.length()) {
+            val jObject = jsonArray.getJSONObject(i)
+            val gson = Gson()
+            val movie = gson.fromJson(jObject.toString(), Movie::class.java)
+            movieList.add(movie)
+        }
+
+        return movieList
+    }
+
+    fun onDestroy() {
+        mainView = null
+    }
+}
