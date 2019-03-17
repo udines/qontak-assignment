576cc16a06cb5eb5387c07dc2852e0ecb0f84cfb
Udin <udines@users.noreply.github.com>
2019-03-18 01:11:56 +0700

    Add function to get credit data in detail Interactor.

--- app/src/main/java/com/qontak/assignment/detailpage/DetailActivityInteractor.kt
+++ app/src/main/java/com/qontak/assignment/detailpage/DetailActivityInteractor.kt
@@ -9,6 +9,7 @@ class DetailActivityInteractor {
 
     interface OnFinishedListener {
         fun onResultSuccess(jsonData: String)
+        fun onCreditResultSuccess(jsonData: String)
         fun onResultFail()
     }
 
@@ -30,6 +31,33 @@ class DetailActivityInteractor {
                 val jsonData = response.body()?.string()
                 if (jsonData != null) {
                     onFinishedListener.onResultSuccess(jsonData)
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                onFinishedListener.onResultFail()
+            }
+
+        })
+    }
+
+    fun getCredit(onFinishedListener: OnFinishedListener, movieId: Int) {
+        val url =
+            Constants.BASE_URL_API + "movie/" + movieId.toString() + "/credits?api_key=" + Constants.API_KEY
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .get()
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("response code", response.code().toString())
+                val jsonData = response.body()?.string()
+                if (jsonData != null) {
+                    onFinishedListener.onCreditResultSuccess(jsonData)
                 }
             }
 
