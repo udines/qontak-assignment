a388b94cb902bd65c5973b6cb316df2434d8cd50
Udin <udines@users.noreply.github.com>
2019-03-17 15:14:25 +0700

    Create detail activity Presenter.

---
+++ app/src/main/java/com/qontak/assignment/detailpage/DetailActivityPresenter.kt
@@ -0,0 +1,30 @@
+package com.qontak.assignment.detailpage
+
+import android.util.Log
+import com.google.gson.Gson
+import com.qontak.assignment.datamodel.MovieDetail
+
+class DetailActivityPresenter(private var detailView: DetailView?, private val detailInteractor: DetailActivityInteractor)
+    : DetailActivityInteractor.OnFinishedListener {
+
+    override fun onResultSuccess(jsonData: String) {
+        detailView?.displayMovieDetail(convertJsonToMovieDetail(jsonData))
+    }
+
+    override fun onResultFail() {
+        Log.e("result", "fail")
+    }
+
+    fun getData(id: Int) {
+        detailInteractor.getMovieDetail(this, id)
+    }
+
+    private fun convertJsonToMovieDetail(jsonData: String): MovieDetail {
+        val gson = Gson()
+        return gson.fromJson(jsonData, MovieDetail::class.java)
+    }
+
+    fun onDestoy() {
+        detailView = null
+    }
+}
--- app/src/main/java/com/qontak/assignment/detailpage/MovieDetailActivity.kt
+++ app/src/main/java/com/qontak/assignment/detailpage/MovieDetailActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import com.qontak.assignment.R
+import com.qontak.assignment.datamodel.MovieDetail
 
 import kotlinx.android.synthetic.main.activity_movie_detail.*
+
+interface DetailView {
+    fun displayMovieDetail(movieDetail: MovieDetail)
+}
 
 class MovieDetailActivity : AppCompatActivity() {
 
