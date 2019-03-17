6ab50cacbbe454ba84fa09c330a434eeb705bd6e
Udin <udines@users.noreply.github.com>
2019-03-18 01:40:22 +0700

    Add constant value for "Director" and "Writing"

--- app/src/main/java/com/qontak/assignment/Constants.kt
+++ app/src/main/java/com/qontak/assignment/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val FILTER_TOP_RATED = "top_rated"
     const val FILTER_POPULAR = "popular"
     const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/"
+    const val MOVIE_CREW_JOB_DIRECTOR = "Director"
+    const val MOVIE_CREW_DEPARTMENT_WRITING = "Writing"
 }
