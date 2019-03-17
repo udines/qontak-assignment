7c96027ef8e12b369c96064784126069845c09d9
Udin <udines@users.noreply.github.com>
2019-03-17 17:56:55 +0700

    Remove useless brackets in Constants class.

--- app/src/main/java/com/qontak/assignment/Constants.kt
+++ app/src/main/java/com/qontak/assignment/Constants.kt
@@ -1,6 +1,6 @@
 package com.qontak.assignment
 
-class Constants() {
+class Constants {
     companion object {
         const val API_KEY = "f6cd08f3369460bc00695cfeda1f1a28"
         const val FILTER_TOP_RATED = "top_rated"
