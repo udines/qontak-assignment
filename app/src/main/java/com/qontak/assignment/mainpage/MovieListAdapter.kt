b65b823ec3466fe44b14f3f80de3cd9ce0bb3e2b
Udin <udines@users.noreply.github.com>
2019-03-17 14:26:44 +0700

    Handle movie item on click in the adapter class.

--- app/src/main/java/com/qontak/assignment/mainpage/MovieListAdapter.kt
+++ app/src/main/java/com/qontak/assignment/mainpage/MovieListAdapter.kt
@@ -1,6 +1,7 @@
 package com.qontak.assignment.mainpage
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +11,26 @@ import android.widget.TextView
 import com.qontak.assignment.GlideApp
 import com.qontak.assignment.R
 import com.qontak.assignment.datamodel.Movie
+import com.qontak.assignment.detailpage.MovieDetailActivity
 import kotlinx.android.synthetic.main.card_movie_grid.view.*
 
 class MovieListAdapter(private val context: Context, private val movieList : ArrayList<Movie>):
     RecyclerView.Adapter<ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         val view = LayoutInflater.from(p0.context).inflate(R.layout.card_movie_grid, p0, false)
-        return ViewHolder(view)
+        val viewHolder = ViewHolder(view)
+
+        //handle movie item on click
+        view.setOnClickListener {
+            val movie = movieList[viewHolder.adapterPosition]
+            val intent = Intent(context, MovieDetailActivity::class.java)
+
+            //pass movie id to movie detail activity
+            intent.putExtra("movieId", movie.id)
+            context.startActivity(intent)
+        }
+
+        return viewHolder
     }
 
     override fun getItemCount(): Int {
