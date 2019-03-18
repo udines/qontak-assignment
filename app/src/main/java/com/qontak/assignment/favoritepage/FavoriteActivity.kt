e10ef6dfa704b7990cae8393e959769359a9d87d
Udin <udines@users.noreply.github.com>
2019-03-18 12:40:52 +0700

    Create basic activity for favorited movie.

---
+++ app/src/main/java/com/qontak/assignment/favoritepage/FavoriteActivity.kt
@@ -0,0 +1,24 @@
+package com.qontak.assignment.favoritepage
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import com.qontak.assignment.R
+
+import kotlinx.android.synthetic.main.activity_favorite.*
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
