0e8bece284eed76568a2b443ecb40bbde6829a65
Udin <udines@users.noreply.github.com>
2019-03-18 13:02:37 +0700

    Remove FAB from favorite activity.

--- app/src/main/java/com/qontak/assignment/favoritepage/FavoriteActivity.kt
+++ app/src/main/java/com/qontak/assignment/favoritepage/FavoriteActivity.kt
@@ -13,11 +13,6 @@ class FavoriteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
