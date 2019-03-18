82bd8b40166313c33002eedb06fbca1ca29775a0
Udin <udines@users.noreply.github.com>
2019-03-18 20:59:01 +0700

    Change filter button color based on selected filter in main activity.

--- app/src/main/java/com/qontak/assignment/mainpage/MainActivity.kt
+++ app/src/main/java/com/qontak/assignment/mainpage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.qontak.assignment.mainpage
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
@@ -9,8 +10,11 @@ import com.miguelcatalan.materialsearchview.MaterialSearchView
 import com.qontak.assignment.Constants
 import com.qontak.assignment.datamodel.Movie
 import kotlinx.android.synthetic.main.activity_main.*
-import com.qontak.assignment.R
+import com.qontak.assignment.authentication.LoginDialog
+import com.qontak.assignment.favoritepage.FavoriteActivity
 import kotlinx.android.synthetic.main.content_main.*
+import android.content.Context
+import com.qontak.assignment.R
 
 
 interface MainView {
@@ -118,6 +122,8 @@ class MainActivity : AppCompatActivity(), MainView {
 
                     //use query data as input to the getData method
                     mainPresenter.getData(query)
+                    mainButtonFilterPopular.setBackgroundResource(R.drawable.background_filter_button)
+                    mainButtonFilterTopRated.setBackgroundResource(R.drawable.background_filter_button)
                 }
                 return false
             }
@@ -136,6 +142,9 @@ class MainActivity : AppCompatActivity(), MainView {
         //page input set to 1 as initial
         mainPresenter.getData(Constants.FILTER_POPULAR)
 
+        //set button color selected for popular filter
+        mainButtonFilterPopular.setBackgroundResource(R.drawable.background_filter_button_selected)
+        mainButtonFilterTopRated.setBackgroundResource(R.drawable.background_filter_button)
         super.onStart()
     }
 
@@ -149,14 +158,38 @@ class MainActivity : AppCompatActivity(), MainView {
         //page is set to 1 as initial
         mainButtonFilterPopular.setOnClickListener {
             mainPresenter.getData(Constants.FILTER_POPULAR)
+            mainButtonFilterPopular.setBackgroundResource(R.drawable.background_filter_button_selected)
+            mainButtonFilterTopRated.setBackgroundResource(R.drawable.background_filter_button)
         }
         mainButtonFilterTopRated.setOnClickListener {
             mainPresenter.getData(Constants.FILTER_TOP_RATED)
+            mainButtonFilterPopular.setBackgroundResource(R.drawable.background_filter_button)
+            mainButtonFilterTopRated.setBackgroundResource(R.drawable.background_filter_button_selected)
         }
 
         //handle load more button
         mainButtonLoadMore.setOnClickListener {
             mainPresenter.getMoreData()
         }
+
+        mainButtonFavorite.setOnClickListener {
+            //check if session id exist
+            if (sessionIdExist()) {
+                startActivity(Intent(this, FavoriteActivity::class.java))
+            } else {
+                showLoginDialog()
+            }
+        }
+    }
+
+    private fun sessionIdExist(): Boolean {
+        val prefs = getSharedPreferences(Constants.PREF_NAME_AUTH, Context.MODE_PRIVATE)
+        val sessionId = prefs.getString(Constants.PREF_KEY_SESSION_ID, null)
+        return sessionId != null
+    }
+
+    private fun showLoginDialog() {
+        val dialog = LoginDialog()
+        dialog.show(supportFragmentManager, "login")
     }
 }
