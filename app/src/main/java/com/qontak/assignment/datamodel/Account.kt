a3ffb22f787eee800c2b4557dd451f5358f8aba9
Udin <udines@users.noreply.github.com>
2019-03-18 23:07:50 +0700

    Create AccountData data class.

---
+++ app/src/main/java/com/qontak/assignment/datamodel/Account.kt
@@ -0,0 +1,21 @@
+package com.qontak.assignment.datamodel
+
+import com.google.gson.annotations.SerializedName
+
+data class AccountData(
+    @SerializedName("avatar") val avatar: Avatar,
+    @SerializedName("id") val id: Int,
+    @SerializedName("include_adult") val includeAdult: Boolean,
+    @SerializedName("iso_3166_1") val iso31661: String,
+    @SerializedName("iso_639_1") val iso6391: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("username") val username: String
+)
+
+data class Avatar(
+    @SerializedName("gravatar") val gravatar: Gravatar
+)
+
+data class Gravatar(
+    @SerializedName("hash") val hash: String
+)
